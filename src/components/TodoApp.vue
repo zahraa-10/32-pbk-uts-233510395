41ffb0170121e7c4dda5f129cf05a19641c9597d
zahraa-10 <shilpaazahra@student.uir.ac.id>
2025-05-06 16:29:21 +0700

    Fungsi menambahkan kegiatan baru

--- src/components/TodoApp.vue
+++ src/components/TodoApp.vue
@@ -4,12 +4,26 @@ import { ref, computed } from 'vue';
 const activities = ref([]);
 const newActivity = ref("");
 const showOnlyIncomplete = ref(false);
+
+// Fungsi menambahkan kegiatan baru
+const addActivity = () => {
+  if (newActivity.value.trim()) {
+    activities.value.push({ text: newActivity.value, completed: false });
+    newActivity.value = "";
+  }
+};
 </script>
 
 <template>
  <div class="todo-app">
+  <div class="todo-input">
+      <input
+        v-model="newActivity"
+        type="text"
+        placeholder="Add New Activities" />
+      <button @click="addActivity">Add</button>
+    </div>
 
-  
  </div>
 
 </template>
