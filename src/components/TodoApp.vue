e60564a9d833198a673785d9e9261410c4f283fc
zahraa-10 <shilpaazahra@student.uir.ac.id>
2025-05-06 16:25:05 +0700

    inisialisasi ToDo List

---
+++ src/components/TodoApp.vue
@@ -0,0 +1,15 @@
+<script setup>
+import { ref, computed } from 'vue';
+
+const activities = ref([]);
+const newActivity = ref("");
+const showOnlyIncomplete = ref(false);
+</script>
+
+<template>
+ <div class="todo-app">
+
+  
+ </div>
+
+</template>
